0f160196de48eea468e34770f1cead9f2fe7d777
Bruno Oberle <b.oberle@zoho.eu>
2022-06-12 20:34:12 +0200

    blackified

--- vinted_downloader.py
+++ vinted_downloader.py
@@ -13,9 +13,14 @@ from selenium.webdriver.firefox.service import Service
 
 from bs4 import BeautifulSoup
 
-SNAP = (1, 2, 3, 4,)
-#SNAP = (6, 7, 8)
-#SNAP = ()
+SNAP = (
+    1,
+    2,
+    3,
+    4,
+)
+# SNAP = (6, 7, 8)
+# SNAP = ()
 
 
 def download(url):
@@ -28,56 +33,56 @@ def download(url):
         return fh.read()
     except urllib.error.URLError:
         print("*** can't read the page `%s' ***" % url)
-        #input("Press Enter to continue...")
+        # input("Press Enter to continue...")
         raise
 
 
 def save(data, dpath, fname, *, binary=True):
     fpath = os.path.join(dpath, fname)
     print(f"Saving '{fpath}'.")
-    open(fpath, 'wb' if binary else 'w').write(data)
-
+    open(fpath, "wb" if binary else "w").write(data)
 
 
 def get_profile_picture_url(soup):
     image_url = None
-    for script in soup.find_all('script', {'data-component-name': "Profile"}):
+    for script in soup.find_all("script", {"data-component-name": "Profile"}):
         data = json.loads(script.string)
-        assert image_url is None # only one
-        photo_data = data['userInfoProps']['user']['photo']
+        assert image_url is None  # only one
+        photo_data = data["userInfoProps"]["user"]["photo"]
         if photo_data is not None:
-            image_url = photo_data['url']
+            image_url = photo_data["url"]
             print(f"Found user profile picture at '{image_url}'.")
         # no break to check if only one
     return image_url
 
+
 def ask_to_save_seller_page_and_picture(url, dpath):
-    seller_dom = os.path.abspath(os.path.join(dpath, 'seller_dom.html'))
+    seller_dom = os.path.abspath(os.path.join(dpath, "seller_dom.html"))
     print(f"Go to {url} and save the DOM to {seller_dom}")
-    seller_photo = os.path.abspath(os.path.join(dpath, 'profile.jpg'))
+    seller_photo = os.path.abspath(os.path.join(dpath, "profile.jpg"))
     print(f"Also, save the picture, if any, to {seller_photo}")
     input("Press Enter when done.")
 
 
 def get_seller_dom_and_picture_url(url, dpath):
     # dom
-    #driver = webdriver.Firefox(executable_path="/tmp/geckodriver")
+    # driver = webdriver.Firefox(executable_path="/tmp/geckodriver")
     service = Service("/tmp/geckodriver")
     driver = webdriver.Firefox(service=service)
     driver.get(url)
     time.sleep(2)
     html = driver.execute_script("return document.body.outerHTML;")
-    with open(os.path.join(dpath, 'seller_dom.html'), "w") as f:
+    with open(os.path.join(dpath, "seller_dom.html"), "w") as f:
         f.write(html)
     driver.close()
     # image
     soup = BeautifulSoup(html, "lxml")
     image_url = None
-    for img in soup.find_all('img'):
-        if '/f800/' not in img['src']:
+    for img in soup.find_all("img"):
+        if "/f800/" not in img["src"]:
             continue
-        assert image_url is None # only one
-        image_url = img['src']
+        assert image_url is None  # only one
+        image_url = img["src"]
         print(f"Found user profile picture at '{image_url}'.")
         # no break to check if only one
     if not image_url:
@@ -85,35 +90,33 @@ def get_seller_dom_and_picture_url(url, dpath):
     return image_url
 
 
-
-
 def download_and_save(url, outdpath):
 
     url = url.rstrip("/")
     parsed = urlparse(url)
-    #input(parsed)
+    # input(parsed)
 
     # dest dir
     dname = os.path.basename(parsed.path)
-    #input(dname)
+    # input(dname)
     assert dname
     dpath = os.path.join(outdpath, dname)
     if os.path.exists(dpath):
         raise FileExistsError(dpath)
     os.makedirs(dpath)
 
-    open(os.path.join(dpath, 'url'), 'w').write(url)
+    open(os.path.join(dpath, "url"), "w").write(url)
 
     # main page
-    main_page = download(url).decode('utf-8')
-    save(main_page, dpath, 'page.html', binary=False)
+    main_page = download(url).decode("utf-8")
+    save(main_page, dpath, "page.html", binary=False)
 
     # item images
     soup = BeautifulSoup(main_page, "lxml")
     div = soup.find("div", class_="item-photos")
     image_urls = []
-    for figure in div.find_all('figure'):
-        image_url = figure.a['href']
+    for figure in div.find_all("figure"):
+        image_url = figure.a["href"]
         print("Found an image at '%s'" % image_url)
         image_urls.append(image_url)
 
@@ -121,67 +124,67 @@ def download_and_save(url, outdpath):
     # page) are in a "u-hidden" div (there are several of this kind of div, we
     # want the one with the 'item-description' figures)
     for u_hidden_div in soup.find_all("div", class_="u-hidden"):
-        for figure in u_hidden_div.find_all('figure', class_="item-description"):
-            image_url = figure.a['href']
+        for figure in u_hidden_div.find_all("figure", class_="item-description"):
+            image_url = figure.a["href"]
             print("Found a supplementary image at '%s'" % image_url)
             image_urls.append(image_url)
 
     assert image_urls
 
-    open(os.path.join(dpath, 'image_urls'), 'w').write("\n".join(image_urls))
+    open(os.path.join(dpath, "image_urls"), "w").write("\n".join(image_urls))
 
     for i, image_url in enumerate(image_urls):
         image_data = download(image_url)
         save(image_data, dpath, f"image_{i}.jpg", binary=True)
 
     # seller url
-    #seller_url = soup.find('span', class_="user-login-name").a['href']
+    # seller_url = soup.find('span', class_="user-login-name").a['href']
     seller_id = soup.find(
-        'script',
-        attrs={'data-component-name':"ItemUserInfo"},
-    ).string;
+        "script",
+        attrs={"data-component-name": "ItemUserInfo"},
+    ).string
     assert seller_id
-    seller_id = json.loads(seller_id)['user']['id']
+    seller_id = json.loads(seller_id)["user"]["id"]
     seller_url = f"/members/{seller_id}"
     seller_url = f"{parsed.scheme}://{parsed.netloc}{seller_url}"
-    #input(seller_url)
+    # input(seller_url)
 
-    open(os.path.join(dpath, 'seller_url'), 'w').write(seller_url)
+    open(os.path.join(dpath, "seller_url"), "w").write(seller_url)
 
     # seller page
-    seller_page = download(seller_url).decode('utf-8')
-    save(seller_page, dpath, 'seller.html', binary=False)
+    seller_page = download(seller_url).decode("utf-8")
+    save(seller_page, dpath, "seller.html", binary=False)
 
     # seller profile picture
-    #soup = BeautifulSoup(seller_page, "lxml")
-    #profile_picture_url = get_profile_picture_url(soup)
+    # soup = BeautifulSoup(seller_page, "lxml")
+    # profile_picture_url = get_profile_picture_url(soup)
     profile_picture_url = get_seller_dom_and_picture_url(seller_url, dpath)
     if profile_picture_url is not None:
-        open(os.path.join(dpath, 'profile_picture_url'), 'w').write(
-            profile_picture_url)
+        open(os.path.join(dpath, "profile_picture_url"), "w").write(profile_picture_url)
         profile_picture_data = download(profile_picture_url)
         save(profile_picture_data, dpath, f"profile_picture.jpg", binary=True)
 
 
-
 def parse_args():
     # definition
-    parser = argparse.ArgumentParser(prog="progname",
+    parser = argparse.ArgumentParser(
+        prog="progname",
         description="what the program does",
-        formatter_class=argparse.RawDescriptionHelpFormatter)
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
     # arguments (not options)
-    #parser.add_argument("infpaths", nargs="+", help="input files")
+    # parser.add_argument("infpaths", nargs="+", help="input files")
     parser.add_argument("url", default="", help="url")
-    #parser.add_argument("outfpath", default="", help="output file")
+    # parser.add_argument("outfpath", default="", help="output file")
     # options
-    #parser.add_argument("--swith", dest="switch", default=False,
+    # parser.add_argument("--swith", dest="switch", default=False,
     #   action="store_true", help="")
-    parser.add_argument("-d", dest="outdpath", required=False,
-        default="output", help="output directory")
+    parser.add_argument(
+        "-d", dest="outdpath", required=False, default="output", help="output directory"
+    )
     # reading
     args = parser.parse_args()
     return args
-
 
 
 def main():
@@ -191,6 +194,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
