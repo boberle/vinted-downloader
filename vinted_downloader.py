171f9f6f310c7a809f2553d6c9882235cb9dc70e
Bruno Oberle <bruno@boberle.com>
2024-03-16 10:04:17 +0100

    update user agent to Chrome 122

--- vinted_downloader.py
+++ vinted_downloader.py
@@ -13,7 +13,7 @@ from typing import Any, cast, Protocol, Generator
 
 import requests
 
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
 SNAP = [1, 2, 3]
 
 
